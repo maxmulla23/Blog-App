5d294c16833d12064fe0378b22cd9cf10fa17d07
maxmulla23 <maxmulla23@gmailcom>
2024-01-07 12:07:25 +0300

    data

--- Data/BlogAppContext.cs
+++ Data/BlogAppContext.cs
@@ -9,6 +9,18 @@ namespace BlogApp.Data
 {
     public class BlogAppContext : DbContext
     {
+        protected readonly IConfiguration Configuration;
+        public BlogAppContext (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+  
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"));
+        }
 
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
