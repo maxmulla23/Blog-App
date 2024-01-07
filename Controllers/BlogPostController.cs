96ae233e909408657247b32c2b3d2e6b17c27298
maxmulla23 <maxmulla23@gmail.com>
2024-01-07 18:15:20 +0300

    post new blog

--- Controllers/BlogPostController.cs
+++ Controllers/BlogPostController.cs
@@ -1,6 +1,8 @@
 using System;
 using BlogApp.Data;
+using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers;
 
@@ -12,5 +14,27 @@ public class BlogPostController : Controller
     {
         _context = context;
     }
-    
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.BlogPosts.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("id,Content,CreationTime")] BlogPost blogPost)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(blogPost);
+            blogPost.CreationTime = DateTime.Now;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(blogPost);
+    }
 }
