27669177f45ea8c6f421a377d6f8300f1fe5a41d
maxmulla23 <maxmulla23@gmail.com>
2024-01-08 12:13:11 +0300

    blogpost controller action done

--- Controllers/BlogPostController.cs
+++ Controllers/BlogPostController.cs
@@ -6,6 +6,7 @@ using BlogApp.Data;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BlogApp.Controllers;
 
@@ -17,12 +18,28 @@ public class BlogPostController : Controller
     {
         _context = context;
     }
-    
+    //Get all Blogs
     public async Task<IActionResult> Index()
     {
         return View(await _context.BlogPosts.ToListAsync());
     }
 
+    //get blog details
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var blogPost = await _context.BlogPosts
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+
+        return View(blogPost);
+    }
     public IActionResult Create()
     {
         return View();
@@ -40,5 +57,83 @@ public class BlogPostController : Controller
             return RedirectToAction(nameof(Index));
         }
         return View(blogPost);
+    }
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var blogPost = await _context.BlogPosts.FindAsync(id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+        return View(blogPost);
+    }
+
+    [HttpPut]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Edit (int id, [Bind("id,CreationTime, Content")] BlogPost blogPost)
+    {
+        if (id != blogPost.Id)
+        {
+            return NotFound();
+        }
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(blogPost);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(blogPost.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(blogPost);
+    }
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var blogPost = await _context.BlogPosts
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+
+        return View(blogPost);
+    }
+    [HttpDelete]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var blogPost = await _context.BlogPosts.FindAsync(id);
+        if (blogPost != null)
+        {
+            _context.BlogPosts.Remove(blogPost);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool BlogPostExists(int id)
+    {
+        return _context.BlogPosts.Any(e => e.Id == id);
     }
 }
